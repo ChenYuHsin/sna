ea24b75c9414bb55d22085d57b94db8b4ccdf6ad
Tommy <cchccy1623@gmail.com>
2014-12-31 10:38:25 +0800

    fblogin

--- js/index.js
+++ js/index.js
@@ -16,5 +16,27 @@ jQuery(document).ready(function($){
 			}
 		});
 	});
+
+	Parse.initialize("i3YYpkGy0zHRuBevYamiXHNZIGQO8Mmj7IjUxGXE", "sHviJS2dqoTQWIPM3Fx3Si2zv01YQ9KgMIQXMun5");
+
+	//fblogin button
+	$("#my-login-button").click(function(){
+	    Parse.FacebookUtils.logIn("user_friends", {
+	        success: function(user) {
+	            if (!user.existed()){
+	                alert("User signed up and logged in through Facebook!");
+	                var fbid = user.get('authData')['facebook'].id;
+	                user.set('facebookid', fbid);
+	                user.save();
+	            } 
+	            else{
+	                alert("User logged in through Facebook!");
+	            }
+	        },
+	        error: function(user, error) {
+	            alert("User cancelled the Facebook login or did not fully authorize.");
+	        }
+	    });
+	});
 	
 });
