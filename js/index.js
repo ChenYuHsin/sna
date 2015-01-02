0c1f5b7a35bcd8e7203edbc4332d7b48a3be9da3
Lisa <100306056@nccu.edu.tw>
2015-01-02 23:48:47 +0800

    modify fb img

--- js/index.js
+++ js/index.js
@@ -92,17 +92,20 @@ jQuery(document).ready(function($){
 
 /********************************* 朋友的timeline *****************************************/
 
-var friendsTimlineSection = function(){
-	var friendsSection = "<section id='cd-timeline' class=' no_  cd-container two wide column center' style='position: relative'>"+
+/*var friendsTimlineSection = function(num){
+	var friendsSection = "<section id='cd-timeline' class=' no_" +num + " cd-container two wide column center' style='position: relative'>"+
 								"<img src='img/7.jpg' alt='Picture' class='friends_pic' >"+
 							"</section>";
 	return friendsSection;
-}
+}*/
 var currentUser = Parse.User.current();
 if (currentUser) {
 	var friends = currentUser.get("friends");
 	for(i = 0;i < friends.length; i++){
-		$("#friends_timmeline_area .center").append(friendsTimlineSection());
+		var friendsSection = "<section id='cd-timeline' class=' no_" +i + " cd-container two wide column center' style='position: relative'>"+
+								"<img src='img/7.jpg' alt='Picture' class='friends_pic' >"+
+							"</section>";
+		$("#friends_timmeline_area .center").append(friendsSection);
 		/*var Dent = Parse.Object.extend("Dent");
 		var dent = new Parse.Query(Dent);
 		dent.equalTo("poster2", friends[i]);
