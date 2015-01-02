3b3eb36fc3e6181b7a9e6473d4c5b6c583abc18c
Lisa <100306056@nccu.edu.tw>
2015-01-03 00:16:32 +0800

    modify fb img

--- js/index.js
+++ js/index.js
@@ -110,12 +110,11 @@ if (currentUser) {
 		var Dent = Parse.Object.extend("Dent");
 		var dent = new Parse.Query(Dent);
 		dent.equalTo("poster2", friends[i]);
+		var className= ".no_"+i;
 		dent.find({
 		  success: function(results) {
 		    alert("Successfully retrieved " + results.length );
 		    // Do something with the returned Parse.Object values
-		    var className= ".no_"+i;
-		    alert(i);
 		    $(className).append(timeLineTpl(1, 3, 'smile' ,'yellow'));
 		  },
 		  error: function(error) {
