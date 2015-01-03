f11e8534e6990b25e0f9a965a9dc40622cc61574
Lisa <100306056@nccu.edu.tw>
2015-01-03 17:05:31 +0800

    modify fb img

--- js/index.js
+++ js/index.js
@@ -136,7 +136,7 @@ if (currentUser) {
 	for(var i = 0;i < friends.length; i++){
 		var queryFriend = new Parse.Query(Parse.User);
 		queryFriend.equalTo("objectId", friends[i]);
-		alert(friends[i]));  // find all the women
+		alert(friends[i]);  // find all the women
 		queryFriend.find({
 			success: function(result) {
 				//var d =result.get("imagesrc");
