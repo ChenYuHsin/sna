cdeb3bc87058a3812127dbff6c9ad85708cbe6d1
s914211 <cchccy1623@gmail.com>
2015-01-03 19:58:33 +0800

    profile recommend friend

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
 
-	var friendbase = function(id, url, name){
-		var friendbody = $('<div class="card" id="1234567" data-roleId="'+id+'"><div class="dimmable image"><div class="ui dimmer"><div class="content"><div class="center"><div class="ui inverted button add_friend_btn">Add Friend</div></div></div></div><img src="'+url+'"></div><div class="content"><a class="header">'+name+'</a></div></div>');
-		$('#recommendfriend').append(friendbody);
-	}
 })
