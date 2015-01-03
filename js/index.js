4143b76919655efd12251eb0e5691462829ddff8
Lisa <100306056@nccu.edu.tw>
2015-01-03 13:58:29 +0800

    modify fb img

--- js/index.js
+++ js/index.js
@@ -199,7 +199,7 @@ if (currentUser) {
 					var calkeep = (dent_end.getTime() - dent_start.getTime())/1800000;
 
 					
-					origin += "<tr><td>" + dent_poster + "</td><td>" + dent_category + "</td><td>" + dent_content + "</td><td>" + dent_start + "</td><td>" + dent_end + "</td><td><a href='response.html?id=" + dent.id + "'>Link</a></td><td><button onclick='like(\"" + dent.id + "\")'>Like</button></td></tr>";
+					//origin += "<tr><td>" + dent_poster + "</td><td>" + dent_category + "</td><td>" + dent_content + "</td><td>" + dent_start + "</td><td>" + dent_end + "</td><td><a href='response.html?id=" + dent.id + "'>Link</a></td><td><button onclick='like(\"" + dent.id + "\")'>Like</button></td></tr>";
 				}
 				var ClassName = "[data-meId = "+ dent_poster.id +"]";
 				console.log(ClassName);
