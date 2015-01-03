8495f6028a59ca41417bb2aab0b54d2eb60d610b
Lisa <100306056@nccu.edu.tw>
2015-01-03 18:35:04 +0800

    modify timeline

--- js/index.js
+++ js/index.js
@@ -122,10 +122,10 @@ if (currentUser) {
 			success: function(result) {
 				//var d =result.get("imagesrc");
 			
-		    //var friendsSection = "<section id='cd-timeline' class=' no_" +i + " cd-container two wide column center' style='position: relative' data-timelineId='"+friends[i]+"'>"+
-			//					"<img src='"+ result.get('imagesrc')+"' alt='Picture' class='friends_pic'>"+
-			//				"</section>";
-			//$("#friends_timmeline_area #1 .content").append(friendsSection);
+		    var friendsSection = "<section id='cd-timeline' class=' no_" +i + " cd-container two wide column center' style='position: relative' data-timelineId='"+friends[i]+"'>"+
+								"<img src='"+ result.get('imagesrc')+"' alt='Picture' class='friends_pic'>"+
+							"</section>";
+			$("#friends_timmeline_area #1 .content").append(friendsSection);
 		  	},
 		  	error: function(){
 		  		alert("error");
