1a0da0182bbed923f05649a257c11dd70b5a0fb1
Lisa <100306056@nccu.edu.tw>
2015-01-04 16:29:27 +0800

    profile

--- js/index.js
+++ js/index.js
@@ -243,7 +243,7 @@ if (currentUser) {
 		var post_id = $(this).closest('cd-timeline-block').attr('id');
 		var Dent = Parse.Object.extend("Dent");
 		var query = new Parse.Query(Dent);
-		query.find({
+		query.find(post_id,{
 			success: function(result){
 				$("#poster_modal_img").attr("src", result.get("poster_img"));
 				$("#poster_modal_name").text(result.gey("poster_name"));
