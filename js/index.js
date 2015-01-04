cb02ffc679707ebc16295f9e483fef05b4ec8439
Lisa <100306056@nccu.edu.tw>
2015-01-04 12:27:41 +0800

    profile

--- js/index.js
+++ js/index.js
@@ -194,7 +194,7 @@ if (currentUser) {
 				for(var i=0; i<results.length; i++){
 
 					var dent = results[i];
-					
+					var dent_poster_obj = dent.get("poster");
 					var dent_poster = dent.get("poster").id;
 					var dent_category = dent.get("category");
 					var dent_content = dent.get("content");
@@ -203,14 +203,22 @@ if (currentUser) {
 					var calstart = dent_start.getHours();
 					var calkeep = (dent_end.getTime() - dent_start.getTime())/3600000;
 					var ClassName = "[data-timelineid = '"+ dent_poster +"']";
-					
-					var poster_query  = new Parse.Query(Parse.User);
-					poster_query.get(dent_poster, {
+					var query_poster = new Parse.Query(Parse.User);
+					query_poster.get(dent_poster, {
 					  success: function(result) {
-						var popupTplCotent = "<div class='ui items popup_item'>"+
+					    // The object was retrieved successfully.
+					    console.log(result.get('imagesrc'));
+					  },
+					  error: function(object, error) {
+					    // The object was not retrieved successfully.
+					    // error is a Parse.Error with an error code and message.
+					    alert();
+					  }
+					});
+					var popupTplCotent = "<div class='ui items popup_item'>"+
 								  "<div class='item'>"+
 								    "<a class='ui tiny image'>"+
-								      "<img src='"+result.get('imagesrc')+" ' style='border-radius: .25rem;'>"+
+								      "<img src='"+dent_poster_obj.get('imagesrc')+" ' style='border-radius: .25rem;'>"+
 								    "</a>"+
 								    "<div class='content'>"+
 								      "<a class='author'>Joe Henderson</a>"+
@@ -224,17 +232,9 @@ if (currentUser) {
 								    "</div>"+
 								  "</div>";
 							
-						$(ClassName).append(timeLineTpl( dent_poster,calstart, calkeep, dent_category  ,'yellow'));
-						var popuoClass = "."+dent_poster+" "+".cd-timeline-img";
-						$(popuoClass).attr("data-html", popupTplCotent).popup({on: "hover"});
-					  },
-					  error: function(object, error) {
-					    // The object was not retrieved successfully.
-					    // error is a Parse.Error with an error code and message.
-					    console.log(object+" "+error);
-					  }
-					});
-					
+					$(ClassName).append(timeLineTpl( dent_poster,calstart, calkeep, dent_category  ,'yellow'));
+					var popuoClass = "."+dent_poster+" "+".cd-timeline-img";
+					$(popuoClass).attr("data-html", popupTplCotent).popup({on: "hover"});
 					
 					//origin += "<tr><td>" + dent_poster + "</td><td>" + dent_category + "</td><td>" + dent_content + "</td><td>" + dent_start + "</td><td>" + dent_end + "</td><td><a href='response.html?id=" + dent.id + "'>Link</a></td><td><button onclick='like(\"" + dent.id + "\")'>Like</button></td></tr>";
 				}
