392d65ed3707875c88bae2f35b2f7a50654d1668
Lisa <100306056@nccu.edu.tw>
2015-01-05 17:02:34 +0800

    timeline

--- js/index.js
+++ js/index.js
@@ -212,7 +212,7 @@ if (currentUser) {
 					var getstartHour = dent_start.getHours();
 					var getstartMinutes = dent_start.getMinutes();
 					var calmarginTop = (getstartHour-8)*60*2 + getstartMinutes*2;
-					console.log(calkeep);
+					console.log(dent_end.getTime());
 					$(ClassName).append(timeLineTpl(dent_poster,calmarginTop, calkeep, dent_category  ,'yellow', dent.id));
 					var popupTplCotent = "<div class='ui items popup_item'>"+
 								  "<div class='item'>"+
