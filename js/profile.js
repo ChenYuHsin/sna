1a567b08d88831d8e1c97db6f89a0fcfcc8b5fd3
Tommy <cchccy1623@gmail.com>
2015-01-06 16:15:49 +0800

    add friend

--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-	console.log($('#addfriendbtn'));
-	$('#addfriendbtn').on("click",function(){
-		var friendid = $(this).parent('data-roleid').text();
-		var friendname = $(this).parent().children('.content').children('.header').text();
-		console.log(friendid);
-		console.log(friendname);
-		var r = confirm("Do you want to add " + friendname + " as a friend?");
-		if (r==true){
-			var currentuserfriends = Parse.User.current().get('friends');
-			currentuserfriends.push(friendid);
-			currentuserfriends.save();
-		}
-		else{
-			alert("Please consider before you press the button!");
-		}
-	})
+	var addfriend = function(){
+		$('#add_friend_btn').on("click",function(){
+			var friendid = $(this).parent('data-roleid').text();
+			var friendname = $(this).parent().children('.content').children('.header').text();
+			console.log(friendid);
+			console.log(friendname);
+			var r = confirm("Do you want to add " + friendname + " as a friend?");
+			if (r==true){
+				var currentuserfriends = Parse.User.current().get('friends');
+				currentuserfriends.push(friendid);
+				currentuserfriends.save();
+			}
+			else{
+				alert("Please consider before you press the button!");
+			}
+		})
+	}
 })
+
+$(document).on("click", "#add_friend_btn", addfriend);
