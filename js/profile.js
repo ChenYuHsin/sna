a46555fd3ca0d3cc45e2f18b59a7c9d9d363e5ad
Tommy <cchccy1623@gmail.com>
2015-01-06 16:31:11 +0800

    add friend

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
 $(document).on("click", "#add_friend_btn", function(){
 	$('#add_friend_btn').on("click",function(){
-		var friendid = $(this).parent().attr('data-roleid').text();
+		var friendid = $(this).parent().attr('data-roleId').text();
 		console.log(friendid);
 		console.log(friendname);
 		var r = confirm("Do you want to add " + friendname + " as a friend?");
