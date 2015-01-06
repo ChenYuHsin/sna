27bc384ebe6ff28bfba9d4ee840f488a0466819d
Tommy <cchccy1623@gmail.com>
2015-01-06 22:17:55 +0800

    test timelineid

--- js/index.js
+++ js/index.js
@@ -131,9 +131,8 @@ if (currentUser) {
 				$("#friends_timmeline_area #1 .content").append(friendsSection);
 				queryDent();
 				$(".dinner").css("border", "1px solid black");
-				$(".dinner").each(function(){
-					$(this).attr('data-timelineid');
-				})
+				console.log(jQuery("[class=dinner]"));
+				console.log(jQuery("[class=dinner]").attr('data-timelineid')); 
 				console.log($(".dinner").attr('data-timelineid'));
 				
 			},
