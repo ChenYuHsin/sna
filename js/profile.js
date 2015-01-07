17143eff73d4d28885b1b60b37e57d12061f9bca
Tommy <cchccy1623@gmail.com>
2015-01-07 19:35:12 +0800

    profile little change

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,12 @@
 $(document).on("click", "#add_friend_btn", function(){
+	var recommendfriend = function(id, url, name){
+		var friendbody = $('<div class="card" id="'+id+'"><div class="dimmable image"><div class="ui dimmer"></div><img src="'+url+'"></div><div class="content"><a class="header">'+name+'</a></div><div class="ui bottom blue attached button" id="add_friend_btn"><i class="add icon"></i>Add Friend</div></div>');
+		$('#recommendfriend').append(friendbody);
+	}
+	var friendlist = function(id, url, name){
+		var listbody = $('<div class="card" id="'+id+'"><div class="image"><img src="'+url+'"></div><div class="content"><a class="header">'+name+'</a></div></div>');
+		$('#friendlists').append(listbody);
+	}
 	var friendid = $(this).parent().attr('id');
 	var friendname = $(this).parent().children('.content').children('.header').text();
 	var r = confirm("Do you want to add " + friendname + " as a friend?");
@@ -8,8 +16,17 @@ $(document).on("click", "#add_friend_btn", function(){
 		currentuserfriends.push(friendid);
 		currentuser.set("friends", currentuserfriends);
 		currentuser.save();
-		setTimeout(function(){ 
-			window.location.assign("profile3.html");
-		}, 1500);
+		var user = Parse.Object.extend("User");
+		var query = new Parse.Query(user);
+		query.equalTo("objectId", friendid);
+		query.first({
+			success:function(frienddata){
+				var friendid = frienddata.id;
+				var friendurl = frienddata.get('imagesrc');
+				var friendname = frienddata.get('name');
+				friendlist(friendid, friendurl, friendname);
+				$('#recommendfriend').children('[id="'+friendid+'"]').remove();
+			}
+		})
 	}
 });
