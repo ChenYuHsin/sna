487c2a644962caf3de4b1345657c4a3d9f15d94a
Lisa <100306056@nccu.edu.tw>
2015-01-08 00:52:50 +0800

    merge

--- js/index.js
+++ js/index.js
@@ -128,11 +128,115 @@ if (currentUser) {
 				
 				//console.log($('[id="cd-timeline"]'));
 				//console.log($('[id="cd-timeline"]').length);
-			console.log($('[id="cd-timeline"]').attr('data-timelineid'));
+				//console.log($('[id="cd-timeline"]').attr('data-timelineid'));
 				
-				console.log(jQuery("[class=dinner][1]").attr('data-timelineid')); 
-				console.log($(".dinner").attr('data-timelineid'));
-				var timelineArray = [];
+				//console.log(jQuery("[class=dinner][1]").attr('data-timelineid')); 
+				//console.log($(".dinner").attr('data-timelineid'));
+		/*		var timelineArray = [];
+				$('[id="cd-timeline"]').each(function(){
+					console.log($(this).attr('data-timelineid'));
+					var timelineid = $(this).attr('data-timelineid');
+					
+					var timeline_obj = $(this).attr('data-timelineid');
+					timelineArray.push(timeline_obj);
+				});
+				console.log(timelineArray);                   
+				for(i=0; i<timelineArray.length; i++){
+					var further = "[data-block='"+timelineArray[i]+"']";
+					console.log(further);
+					$('[data-block="cveJoqYgE5"]').each(function(){
+						console.log($(this).attr('id'));
+					})             
+				}
+				var arr = [];
+				setTimeout(function(){
+					$.each( timelineArray, function( index, value ) {
+					  var obj = timelineArray[index];
+					  var objclass = "."+obj;
+					  var tt= "[data-block="+value+"]";
+					  var arrid=[];
+					  $(tt).each(function(){
+					  
+					  	
+					  	var dd = $(this).css("margin-top");
+					  	arrid.push(dd);
+					  	//console.log($(tt));
+					  	var top = $(this).prevAll();
+					  	var arrarr=[];//前面的top
+					  	$(top).each(function(){
+					  		arrarr.push($(this).css("margin-top"));
+
+					  	})
+					  	var prevMargintop = $(this).css("margin-top");
+					  	var sumTop = 0;
+					  	for(i=0; i<arrarr.length; i++){
+					  		var num = parseInt(arrarr[i],10);
+					  		var toNumber = parseFloat(num);
+					  		sumTop=sumTop+toNumber;
+					  	}
+					  	console.log(sumTop);
+					  	var parseMinus = parseInt(prevMargintop,10);
+					  	var originMarginTop = parseInt(dd,10);
+					  	var minus = parseFloat(originMarginTop )-parseFloat(parseMinus);
+					  	console.log(minus+" "+parseFloat(originMarginTop )+" "+sumTop);
+					  	$(this).removeAttr("style").css("margin-top",minus+"px");
+					  	
+					  })
+
+					  
+					  arr.push(tt);
+					  //console.log(arrid);
+					  //console.log(arr);
+
+					  	
+					  
+					  var timelineArticle = [];
+					  console.log($(tt).length);
+					})
+
+				},1000)             */
+				
+			},
+			error: function(object, error) {
+				alert(object +" "+error);
+			}
+		});
+		/*queryFriend.equalTo("objectId", friends[i]);
+		queryFriend.find({
+			success: function(result) {
+				
+			    var friendsSection = "<section id='cd-timeline' class=' no_" +i + " cd-container two wide column center' style='position: relative' data-timelineId='"+friends[i]+"'>"+
+									"<img src='' alt='Picture' class='friends_pic'>"+
+								"</section>";
+				$("#friends_timmeline_area #1 .content").append(friendsSection);
+		  	},
+		  	error: function(){
+		  		alert("error");
+		  	}
+		});
+		*/
+
+		var Dent = Parse.Object.extend("Dent");
+		var dent = new Parse.Query(Dent);
+		dent.equalTo("poster2", friends[i]);
+		dent.find({
+		  success: function(results) {
+		    //alert("Successfully retrieved ");
+		    /*for(j = 0 ; j < friends.length ; j++){
+		    	var className= ".no_"+j;
+		    	$(className).append(timeLineTpl(1, 3, 'smile' ,'yellow'));
+		    };*/
+		    // Do something with the returned Parse.Object values
+
+		    
+		  },
+		  error: function(error) {
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+		});
+	}
+	queryDent();
+	var timelineArray = [];
 				$('[id="cd-timeline"]').each(function(){
 					console.log($(this).attr('data-timelineid'));
 					var timelineid = $(this).attr('data-timelineid');
@@ -195,47 +299,6 @@ if (currentUser) {
 					})
 
 				},1000)
-				
-			},
-			error: function(object, error) {
-				alert(object +" "+error);
-			}
-		});
-		/*queryFriend.equalTo("objectId", friends[i]);
-		queryFriend.find({
-			success: function(result) {
-				
-			    var friendsSection = "<section id='cd-timeline' class=' no_" +i + " cd-container two wide column center' style='position: relative' data-timelineId='"+friends[i]+"'>"+
-									"<img src='' alt='Picture' class='friends_pic'>"+
-								"</section>";
-				$("#friends_timmeline_area #1 .content").append(friendsSection);
-		  	},
-		  	error: function(){
-		  		alert("error");
-		  	}
-		});
-		*/
-
-		var Dent = Parse.Object.extend("Dent");
-		var dent = new Parse.Query(Dent);
-		dent.equalTo("poster2", friends[i]);
-		dent.find({
-		  success: function(results) {
-		    //alert("Successfully retrieved ");
-		    /*for(j = 0 ; j < friends.length ; j++){
-		    	var className= ".no_"+j;
-		    	$(className).append(timeLineTpl(1, 3, 'smile' ,'yellow'));
-		    };*/
-		    // Do something with the returned Parse.Object values
-
-		    
-		  },
-		  error: function(error) {
-		    alert("Error: " + error.code + " " + error.message);
-		  }
-		});
-	}
-	queryDent();
     console.log($("[data-timelineid = '8hGeU3b7nd']"));
     console.log($(".dinner").find('.cd-timeline-img'));
     console.log($('.make_dent'));
