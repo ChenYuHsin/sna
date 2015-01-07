8fd631ded915786054e679195691a85663c0d260
Lisa <100306056@nccu.edu.tw>
2015-01-08 00:36:05 +0800

    merge

--- js/index.js
+++ js/index.js
@@ -222,9 +222,9 @@ if (currentUser) {
 		dent.find({
 		  success: function(results) {
 		    //alert("Successfully retrieved ");
-		    for(j = 0 ; j < friends.length ; j++){
+		    /*for(j = 0 ; j < friends.length ; j++){
 		    	var className= ".no_"+j;
-		    	$(className).append(timeLineTpl(1, 3, 'smile' ,'yellow'));
+		    	$(className).append(timeLineTpl(1, 3, 'smile' ,'yellow'));*/
 		    };
 		    // Do something with the returned Parse.Object values
 
