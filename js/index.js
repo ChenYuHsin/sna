c091472fe5a02e9576773811c77523f19665de69
Lisa <100306056@nccu.edu.tw>
2015-01-09 01:43:09 +0800

    debug for response modal

--- js/index.js
+++ js/index.js
@@ -101,7 +101,7 @@ jQuery(document).ready(function($){
 		var dent = new Parse.Query(Dent);
 		dent.find({
 			success: function(results) {
-				postIdArray.push(dent.id);
+				postIdArray.push(dent.get("objectId");
 				console.log(dent.id);
 			},
 			error: function(error) {
