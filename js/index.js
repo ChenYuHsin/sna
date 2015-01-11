d370243d6fe964d149ed7c2be81a01769ffeaf87
林庭弘 <contemplation8213@gmail.com>
2015-01-12 03:01:03 +0800

    fuck

--- js/index.js
+++ js/index.js
@@ -599,17 +599,24 @@ function showStatus(total_status){
 	// 套入 template
 	// append
 	var status_section = $("#status_content");
-	status_section.append("hi");
 	for(var i=0; i<total_status.length; i++){
-		console.log(total_status[i].user.id);
+		console.log(total_status[i].user.get("name"));
+		console.log(total_status[i].user.get("imagesrc"));
+		console.log(total_status[i].user.get("friends"));
 		console.log(total_status[i].dent.id);
 		console.log(total_status[i].createdTime);
 		console.log(total_status[i].dent.get("content"));
+		var action;
+		if(total_status.category == "dent"){
+			action = "make a dent";
+		}else{
+			action = "reply on your dent";
+		}
 		var template = '<div class="event">' + 
 	    	'<div class="label"><img src="'+ total_status[i].user.get("imagesrc") +'"></div>' + 
 	    	'<div class="content">' + 
 	      		'<div class="summary">' + 
-	        		'<a class="user">' + total_status[i].user.get("name") + '</a> make a dent' + 
+	        		'<a class="user">' + total_status[i].user.get("name") + '</a> ' + action +
 	        		'<div class="date">' + moment(total_status.createdTime).fromNow() + '</div>'+
 	      		'</div>' +
 	      		'<div class="extra text">' +
@@ -621,7 +628,7 @@ function showStatus(total_status){
     		'</div>' +
   		'</div>';
   		status_section.append(template);
-  		console.log("template:" + template);
+  		// console.log("template:" + template);
 	}
 }
 
