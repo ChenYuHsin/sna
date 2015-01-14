510663549bfb4dc3e845e2035ebed7df255ea9dc
Tommy <cchccy1623@gmail.com>
2015-01-14 10:21:53 +0800

    profile js

--- js/profile.js
+++ js/profile.js
@@ -1,43 +1,29 @@
-$(document).on("click", "#add_friend_btn", function(){
-	var recommendfriend = function(id, url, name){
-		// var friendbody = $('<div class="card" id="'+id+'"><div class="dimmable image"><div class="ui dimmer"></div><img src="'+url+'"></div><div class="content"><a class="header">'+name+'</a></div><div class="ui bottom blue attached button" id="add_friend_btn"><i class="add icon"></i>Add Friend</div></div>');
-		var friendbody = $('<figure id="'+id'"><img src="'+url+'"/><figcaption><span>'+name+'</span><p class="icon-links"><a href="#"><i class="fa fa-comments-o"></i></a><a href="#"><i class="fa fa-heart-o"></i></a></p></figcaption></figure>');
-
-		$('#recommendfriend').append(friendbody);
-	}
-	var friendlist = function(id, url, name){
-		// var listbody = $('<div class="card" id="'+id+'"><div class="image"><img src="'+url+'"></div><div class="content"><a class="header">'+name+'</a></div></div>');
-		var friendbody = $('<figure id="'+id'"><img src="'+url+'"/><figcaption><span>'+name+'</span><p class="icon-links"><a href="#"><i class="fa fa-comments-o"></i></a><a href="#"><i class="fa fa-heart-o"></i></a></p></figcaption></figure>');
-
-		$('#friendlists').append(listbody);
-	}
-	var friendid = $(this).parent().attr('id');
-	var friendname = $(this).parent().children('.content').children('.header').text();
-	var r = confirm("Do you want to add " + friendname + " as a friend?");
-	if (r==true){
-		var currentuser = Parse.User.current();
-		var currentuserfriends = Parse.User.current().get('friends');
-		currentuserfriends.push(friendid);
-		currentuser.set("friends", currentuserfriends);
-		currentuser.save();
-		var user = Parse.Object.extend("User");
-		var query = new Parse.Query(user);
-		query.equalTo("objectId", friendid);
-		query.first({
-			success:function(frienddata){
-				var friendid = frienddata.id;
-				var friendurl = frienddata.get('imagesrc');
-				var friendname = frienddata.get('name');
-				friendlist(friendid, friendurl, friendname);
-				$('#recommendfriend').children('[id="'+friendid+'"]').remove();
-
-				var addfriendevent = Parse.Object.extend("Event");
-				var addfriend = new addfriendevent();
-				addfriend.set("category", "addfriend");
-				addfriend.set("User", currentuser);
-				addfriend.set("targetuser", frienddata);
-				addfriend.save();
+$(document).ready(function(){
+	var follower = Parse.Object.extend("User");
+	var query = new Parse.Query(follower);
+	query.notEqualTo("objectId", Parse.User.current().id);
+	query.find({
+		success:function(alluser){
+			var myfollower = 0;
+			for(i = 0; i<alluser.length; i++){
+				var alluserfriend = alluser[i].get('friends');
+				for(var j = 0; j<alluserfriend.length; j++){
+					if(alluserfriend[j] == Parse.User.current().id){
+						myfollower++;
+					}
+				}
 			}
-		})
-	}
-});
+			$('.follwers-num').html(myfollower);
+		}
+	})
+
+	var dent = Parse.Object.extend("Dent");
+	var query1 = new Parse.Query(dent);
+	query1.equalTo("poster", Parse.User.current());
+	query1.find({
+		success:function(mydent){
+			var mydentnum = mydent.length;
+			$('.Dents-num').html(mydentnum);
+		}
+	})
+})
